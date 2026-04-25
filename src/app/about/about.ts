f14373edfd2452eafdd42b49c18770c7006d62d9
Dimi4Real <135642923+Dimi4Real@users.noreply.github.com>
2026-04-25 14:07:36 +0200

    About page and styling

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatIcon
+  ],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
 export class About {
+   constructor(private dialog: MatDialog) {}
 
+  openFaq() {
+ 
+    alert('FAQ: 1. Kako da naručim? -> Dodajte u korpu i rezervišite. 2. Dostava je 2-3 dana. 3. Plaćanje pouzećem ili karticom.');
+    
+}
 }
